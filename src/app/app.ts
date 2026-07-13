d2e500acb60a2b9030167ca21c15f58d04101187
darioflo <darioflo19@icloud.com>
2026-07-13 15:32:32 -0600

    Eliminación de tareas y ejemplo de computed()

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Task } from './task';
+import { Task } from './models/Task';
 
 @Component({
   selector: 'app-root',
@@ -9,19 +9,21 @@ import { Task } from './task';
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = 'Gestor de tareas';
-
+  protected readonly title = 'Gestor de Tareas';
   tareas = signal<Task[]>([
     { id: 1, titulo: 'Aprender angular', completada: false },
-    { id: 2, titulo: 'Construir un proyecto nuevo', completada: false },
-    { id: 3, titulo: 'Dominar signals', completada: true },
+    { id: 2, titulo: 'Construir proyecto nuevo', completada: false },
+    { id: 3, titulo: 'Dominar signals', completada: false },
   ]);
+  eliminarTarea: boolean = false;
+  idTarea!: number;
+  totalTareas = computed(() => this.tareas().length);
+  tareasPendientes = computed(() => this.totalTareas() - this.tareasCompletadas());
+  tareasCompletadas = computed(() => this.tareas().filter((tarea) => tarea.completada).length);
 
-  agregar(titulo: string): void {
-    const limpio = titulo.trim();
-    if (!limpio) {
-      return;
-    }
+  agregar(tarea: string) {
+    const limpio = tarea.trim();
+    if (!limpio) return;
 
     this.tareas.update((lista) => [
       ...lista,
@@ -29,9 +31,21 @@ export class App {
     ]);
   }
 
+  mostrarAlerta(id: number): void {
+    this.eliminarTarea = true;
+    this.idTarea = id;
+  }
+
+  eliminar(): void {
+    this.eliminarTarea = true;
+    this.tareas.update((lista) => lista.filter((lista) => lista.id !== this.idTarea));
+    this.idTarea = 0;
+    this.eliminarTarea = false;
+  }
+
   toggle(id: number): void {
     this.tareas.update((lista) =>
-      lista.map((t) => (t.id === id ? { ...t, completada: !t.completada } : t)),
+      lista.map((tarea) => (tarea.id === id ? { ...tarea, completada: !tarea.completada } : tarea)),
     );
   }
 }
