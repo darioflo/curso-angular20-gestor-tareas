bd6d9ee90e7722208a88b3b1b310d41766729ca5
darioflo <darioflo19@icloud.com>
2026-07-22 14:56:49 -0600

    Clase 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,51 +1,14 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Task } from './models/Task';
+import { Task } from './features/tasks/Task';
+import { TaskStore } from './features/tasks/task-store';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css',
+  styleUrls: ['./app.css'],
 })
 export class App {
-  protected readonly title = 'Gestor de Tareas';
-  tareas = signal<Task[]>([
-    { id: 1, titulo: 'Aprender angular', completada: false },
-    { id: 2, titulo: 'Construir proyecto nuevo', completada: false },
-    { id: 3, titulo: 'Dominar signals', completada: false },
-  ]);
-  eliminarTarea: boolean = false;
-  idTarea!: number;
-  totalTareas = computed(() => this.tareas().length);
-  tareasPendientes = computed(() => this.totalTareas() - this.tareasCompletadas());
-  tareasCompletadas = computed(() => this.tareas().filter((tarea) => tarea.completada).length);
-
-  agregar(tarea: string) {
-    const limpio = tarea.trim();
-    if (!limpio) return;
-
-    this.tareas.update((lista) => [
-      ...lista,
-      { id: Date.now(), titulo: limpio, completada: false },
-    ]);
-  }
-
-  mostrarAlerta(id: number): void {
-    this.eliminarTarea = true;
-    this.idTarea = id;
-  }
-
-  eliminar(): void {
-    this.eliminarTarea = true;
-    this.tareas.update((lista) => lista.filter((lista) => lista.id !== this.idTarea));
-    this.idTarea = 0;
-    this.eliminarTarea = false;
-  }
-
-  toggle(id: number): void {
-    this.tareas.update((lista) =>
-      lista.map((tarea) => (tarea.id === id ? { ...tarea, completada: !tarea.completada } : tarea)),
-    );
-  }
+  store = inject(TaskStore);
 }
---
+++ src/app/features/tasks/task-store.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { computed, effect, signal } from '@angular/core';
+import { Task } from './Task';
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskStore {
+  public readonly title = 'Gestor de Tareas';
+  STORAGE_KEY = 'tareas';
+  constructor() {
+    effect(() => {
+      localStorage.setItem(this.STORAGE_KEY, JSON.stringify(this.tareas()));
+    });
+  }
+  tareas = signal<Task[]>([
+    { id: 1, titulo: 'Aprender angular', completada: false },
+    { id: 2, titulo: 'Construir proyecto nuevo', completada: false },
+    { id: 3, titulo: 'Dominar signals', completada: false },
+  ]);
+  eliminarTarea: boolean = false;
+  idTarea!: number;
+  totalTareas = computed(() => this.tareas().length);
+  tareasPendientes = computed(() => this.totalTareas() - this.tareasCompletadas());
+  tareasCompletadas = computed(() => this.tareas().filter((tarea) => tarea.completada).length);
+
+  agregar(tarea: string) {
+    const limpio = tarea.trim();
+    if (!limpio) return;
+
+    this.tareas.update((lista) => [
+      ...lista,
+      { id: Date.now(), titulo: limpio, completada: false },
+    ]);
+  }
+
+  mostrarAlerta(id: number): void {
+    this.eliminarTarea = true;
+    this.idTarea = id;
+  }
+
+  eliminar(): void {
+    this.eliminarTarea = true;
+    this.tareas.update((lista) => lista.filter((lista) => lista.id !== this.idTarea));
+    this.idTarea = 0;
+    this.eliminarTarea = false;
+  }
+
+  toggle(id: number): void {
+    this.tareas.update((lista) =>
+      lista.map((tarea) => (tarea.id === id ? { ...tarea, completada: !tarea.completada } : tarea)),
+    );
+  }
+  private cargar(): Task[] {
+    const guardadas = localStorage.getItem(this.STORAGE_KEY);
+
+    if (guardadas) {
+      return JSON.parse(guardadas);
+    }
+    return [
+      { id: 1, titulo: 'Aprender angular', completada: false },
+      { id: 2, titulo: 'Construir un proyecto nuevo', completada: false },
+      { id: 3, titulo: 'Dominar signals', completada: true },
+    ];
+  }
+}
